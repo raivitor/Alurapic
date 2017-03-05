22aacdd9d112b8e4185e9c43441622fc8ce91370
Raí Vitor <raivitor@gmail.com>
2017-03-04 22:48:13 -0300

    teste 2

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ var http = require('http');
 var app = require('./config/express');
 require('./config/database')('raivitor:hehehehe@ds117830.mlab.com:17830/horadoshow');
 //require('./config/database')('localhost/alurapic');
- 
+ /*
 var porta = process.env.PORT || 3000;
 http.createServer(app).listen(porta, function () {
 	console.log('Servidor iniciado');
-})
+})*/
+
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
