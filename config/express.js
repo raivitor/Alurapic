bd8e780d61e30084ada2a82a6ed18d1d9b0063fe
Raí Vitor <raivitor@gmail.com>
2017-03-05 13:54:32 -0300

    Remoção do consign

--- config/express.js
+++ config/express.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var consign = require('consign');
 var bodyParser = require('body-parser');
 var load = require('express-load');
 
@@ -13,15 +12,5 @@ load('models', {
 	.then('api')
 	.then('routes')
 	.into(app);
-
-load()
-	/*
-	consign({
-			cwd: __dirname + '/app'
-		})
-		.include('models')
-		.then('api')
-		.then('routes')
-		.into(app);
-	*/
+	
 module.exports = app;
