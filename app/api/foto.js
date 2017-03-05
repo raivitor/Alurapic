4d919f130566e32bf810a7140342769c4992fe21
Raí Vitor <raivitor@gmail.com>
2017-03-04 21:18:22 -0300

    Crud feito

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,43 +1,60 @@
+var mongoose = require('mongoose');
 var api = {}
-var cont = 2;
-var fotos = [{
-	_id: 1,
-	titulo: 'Leão',
-	url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-}, {
-	_id: 2,
-	titulo: 'Leão 2',
-	url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-}];
+var model = mongoose.model('Foto');
 
 api.lista = function(req, res) {
-	res.json(fotos);
+	model
+		.find({})
+		.then((fotos) => res.json(fotos)
+		, function (error) {
+			res.status(500).json(error);
+		});
 };
 
 api.buscaPorId = function(req, res) {
-	var foto = fotos.find(foto => foto._id == req.params.id);
-	res.json(foto);
-}
+	model
+		.findById(req.params.id)
+		.then(function (foto) {
+			if(!foto) throw Error('Foto não encontrada');
+			res.json(foto);
+		}, function (error) {
+			console.log(error);
+			res.status(404).json(error);
+		});
+};
 
 api.removePorId = function(req, res) {
-	fotos.filter(foto => foto._id != req.params.id);
-	res.sendStatus(204);
-}
+	model
+		.remove({_id: req.params.id})
+		.then(function (argument) {
+			res.sendStatus(204);
+		}, function (error) {
+			console.log(error);
+			res.status(500).json(error);
+		});
+};
 
 api.adiciona = function (res, req) {
 	var foto = res.body;
-	foto._id = ++cont;
-	fotos.push(foto);
-	req.json(foto);
-}
+	model
+		.create(foto)
+		.then(function (foto) {
+			res.json(foto);
+		}, function (error) {
+			console.log(error);
+			res.status(500).json(error);
+		});
+};
 
 api.atualiza = function(req, res){
-	var foto = req.body;
-	var fotoId = req.params.id;
-
-	var indice = fotos.findIndex(foto => foto._id == fotoId);
-	fotos[indice] = foto;
-	res.sendStatus(200);
-}
+	model
+		.findByIdAndUpdate(req.params.id, req.body)
+		.then(function (foto) {
+			res.json(foto);
+		}, function (error) {
+			console.log(error);
+			res.status(500).json(error);
+		});
+};
 
 module.exports = api;
